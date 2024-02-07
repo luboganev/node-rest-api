b178e9bbc0904eebd70f628c4dc16af1f4e874f3
Lyubomir Ganev <lyubomir.ganev@iu.org>
2024-02-07 15:01:58 +0100

    Add person endpoint

--- src/server.js
+++ src/server.js
@@ -1,9 +1,23 @@
 import express from 'express';
+import { people } from './people.js';
 
 let app = express();
 
 app.get('/hello', (req, res) => {
     res.send('Hello World');
+});
+
+app.get('/people', (req, res) => {
+    res.json(people);
+});
+
+app.get('/people/:name', (req, res) => {
+    let person = people.find(person => person.name === req.params.name);
+    if (person) {
+        res.json(person);
+    } else {
+        res.status(404).send('Person not found');
+    }
 });
 
 app.listen(3000, () => {
